3071a9077c858c671cb7e34dd41230f320522659
kumailZaidi12 <zaidikumail12@gmail.com>
2020-09-19 10:45:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs=require('fs')
+
+//fs.writeFileSync('notes.txt','This is Kumail.')
+
+fs.appendFileSync('notes.txt','I am undergradute from Jamia Millia Islamia')
+fs.appendFileSync('notes.txt','.')
