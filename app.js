2f5c0e3ea759cf3e9a71ae88dbb267098360b259
kumailZaidi12 <zaidikumail12@gmail.com>
2020-09-19 10:52:01 +0530

    file system added

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const fs=require('fs')
 
 //fs.writeFileSync('notes.txt','This is Kumail.')
 
-fs.appendFileSync('notes.txt','I am undergradute from Jamia Millia Islamia')
-fs.appendFileSync('notes.txt','.')
+fs.appendFileSync('notes.txt','I am Kumail.')
+fs.appendFileSync('notes.txt',' I live in Kairana')
